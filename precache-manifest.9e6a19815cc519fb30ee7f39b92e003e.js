0139bf3d5fedeae9c5cfbc9a565050890a5a2eba
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-20 18:16:45 +0000

    Deploy PStestCase/PStestCase.github.io to github.com/PStestCase/PStestCase.github.io.git:gh-pages

---
+++ precache-manifest.9e6a19815cc519fb30ee7f39b92e003e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78a7160c3f3ae6a9b7c4b6d37fc4a4b6",
+    "url": "/index.html/index.html"
+  },
+  {
+    "revision": "5a7390c5676a6deea830",
+    "url": "/index.html/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a4f7a541a7f6cfaeab1b",
+    "url": "/index.html/static/js/2.c6a33591.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/index.html/static/js/2.c6a33591.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a7390c5676a6deea830",
+    "url": "/index.html/static/js/main.db0bb8fc.chunk.js"
+  },
+  {
+    "revision": "dd5ac680df01e64e60a2",
+    "url": "/index.html/static/js/runtime-main.236136c9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/index.html/static/media/logo.5d5d9eef.svg"
+  }
+]);
